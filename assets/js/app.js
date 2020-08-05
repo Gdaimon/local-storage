ece540c034de339cae4e8ac8c7478987cc2d0e55
Carlos Andres <darkklitos@gmail.com>
2020-08-05 00:29:40 -0500

    Adicionando github actions

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,4 +13,20 @@ function eventListeners() {
 // Funciones
 function agregarTweet(e) {
   e.preventDefault(e);
+  const textArea = document.querySelector('#tweet');
+  const tweet = textArea.value.trim();
+  adicionarItem(tweet);
+  textArea.value = '';
+}
+
+function adicionarItem(texto) {
+  if (texto) {
+    const item = document.createElement('li');
+    const botonBorrar = document.createElement('a');
+    botonBorrar.textContent = 'X';
+    botonBorrar.className = 'borrar-tweet';
+    item.textContent = texto;
+    item.appendChild(botonBorrar);
+    listaTweets.appendChild(item);
+  }
 }
